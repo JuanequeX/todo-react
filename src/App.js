661af37de004f43f8d5364d78f8afabc2e109f7d
juanequex <ramon.g.uz37@gmail.com>
2023-01-03 21:49:12 -0600

    Ultimate cleaning of the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App(props) {
   return (
     <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Hello, {props.subject}!
       </p>
